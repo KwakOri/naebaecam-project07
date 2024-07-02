7c8b92fb9ea29b700e149b191e2fcf978306c695
kwakjaehun <jh5414092@icloud.com>
2024-07-03 01:06:23 +0900

    Feat : 메인페이지 리스트 컴포넌트, 카드 컴포넌트 작성

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,5 +1,19 @@
-const PokemonList = () => {
-  return <div>PokemonList</div>;
+import { Pokemon } from "@/types/types.pokemons";
+import Link from "next/link";
+import PokemonCard from "./PokemonCard";
+
+const PokemonList = ({ pokemons }: { pokemons: Pokemon[] }) => {
+  return (
+    <div className="w-full grid grid-cols-6 gap-4">
+      {pokemons.map((pokemon) => {
+        return (
+          <Link href={`/${pokemon.id}`} key={pokemon.id}>
+            <PokemonCard pokemon={pokemon} />
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PokemonList;
